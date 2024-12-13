57fe879180234fa5ec6cf2f680d0910170418c7f
Smart-think-samsu <smartthink.shamsurzzaman@gmail.com>
2024-12-13 22:49:20 +0600

    template add

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\frontend\FrontendController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [FrontendController::class, "index"]);
